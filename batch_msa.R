76591f1abeda78fa0b8c6c9c4c303483f4620c90
SiuShingWong <isaacwongsiushing@gmail.com>
2021-12-14 16:08:33 +0000

    change R

--- batch_msa.R
+++ batch_msa.R
@@ -1,8 +1,7 @@
 library(Biostrings)
 library(msa)
 
-# path = "C:/Users/extin/Dropbox/DPhil in Molecular Cell Biology/data/Bioinformatics/Conservation across different Drosophila species/Data"
-path = "C:/Users/extin/Dropbox/DPhil in Molecular Cell Biology/data/Bioinformatics/Viewing different organisms/Mammals"
+path = "xxx"
 
 temp <- list.files(path, pattern="[a-zA-Z]+", full.names = TRUE)
 files = character()
@@ -19,9 +18,8 @@ for (i in 1:length(files)){
   print (i)
   file <- files[i]
   setwd(save_paths[i])
-  #save_name <- paste(save_paths[i], 'aln.pdf', sep='/')
   aa <- readAAStringSet(file)
   aln <- msa(aa)
   try(msaPrettyPrint(aln, output='pdf', askForOverwrite=FALSE, showLogo="top", shadingMode="functional", shadingModeArg="chemical", verbose=TRUE), silent=TRUE)
-  # msaPrettyPrint(aln, output='pdf', askForOverwrite=FALSE, showLogo="top", shadingMode="functional", shadingModeArg="chemical", verbose=TRUE)
+
 }
